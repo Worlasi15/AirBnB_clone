262c50e9a8193313cdd7f7c0b761f1a7efc5761b
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-09-12 10:06:35 -0700

    pycodestyle check

---
+++ partner.py
@@ -0,0 +1,13 @@
+def partner(names):
+    """
+    Accept project partner.
+
+
+    Args:
+    names (str): The name of my partner.
+
+
+    Returns:
+    str: Acceptance.
+    """
+    return f"Hello, {names}!"
