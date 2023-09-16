03e1042595fe7c05d6bb701b35dd9e2065dbe8de
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-09-16 16:25:44 -0700

    the pycodestyle checks

--- partner.py
+++ partner.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-def Clc_function(Reb1, Reb2):
-result = Reb1 +Reb2
-print("Reb1 and Reb2 accumulates as:", result)
-=======
 #!/usr/bin/env python3
-#A list of collaborators
-
-
-print("collaborator name")
->>>>>>> 9ee314800d15121f1a885e0988eb056401c2df1f
+def Clc_function(Reb1, Reb2):
+    result = Reb1 + Reb2
+    print("Reb1 and Reb2 accumulates as:", result)
