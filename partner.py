9755756e2598728fa1ee9f2dd23f39af8a4cbce0
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-09-16 16:53:42 -0700

    the pycodestyle checks

--- partner.py
+++ partner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
-def Clc_function(10, 20):
-    result = 10 + 20
-    print("10 and 20 accumulates as:", result)
+# This script displays the PID and process name of processing matching bash
+
+# Use pgrep to find processing that matches
+pgrep - l bash
