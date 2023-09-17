8ba7041a1eaf460966c6f19dbef015d93c37be78
0793rebecca <rebeccafrempongmensah@gmail.com>
2023-09-17 16:49:20 -0700

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,18 +3,16 @@
 """
 Defines Amenity class
 """
-
 from models.base_model import BaseModel
+
 
 class Amenity(BaseModel):
     """Representing Amenity object
-
     Attributes:
-        name (str): name of amenity
+    name (str): name of amenity
     """
 
     def __init__(self, *args, **kwargs):
         """Initialize Amenity instance"""
         super().__init__(*args, **kwargs)
         self.name = ""
-
